2324fb789f43341bf57c7ac028f2df44a34ff275
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-03-18 23:25:50 +0100

    add navbar division of divs

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -12,7 +12,16 @@ const Navbar = (props: Props) => {
         <div
         className={`${flexBetween} fixed top-0 z-30 w-full py-6`}
         >
+            <div className={`${flexBetween} mx-auto w-5/6`}>
+                <div className={`${flexBetween} w-full gap-16`}>
+                    {/*LEFT SIDE*/}
+                    <img src={Logo} alt="logo" />
 
+                     {/*RIGHT SIDE*/}
+                     <div className={`${flexBetween} w-full`}></div>
+                </div>
+            </div>
+            
         </div>
 
     </nav>
