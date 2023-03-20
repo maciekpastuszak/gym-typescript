7cef9ca856bb91b2c96c3646ff015e2fbdd181b1
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-03-20 10:03:28 +0100

    refactor Navbar

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import {Bars3Icon, XMarkIcon} from "@heroicons/react/24/solid"
 import Logo from "@/assets/Logo.png";
-import Link from "@/scenes/navbar/Link";
+import Link from "./Link";
 import { SelectedPage } from "@/shared/types";
 import useMediaQuery from "@/hooks/useMediaQuery";
 import ActionButton from "@/shared/ActionButton";
@@ -10,13 +10,13 @@ type Props = {
     isTopOfPage: boolean;
     selectedPage: SelectedPage;
     setSelectedPage: (value: SelectedPage) => void;
-}
+};
 
 const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }: Props) => {
     const flexBetween = "flex items-center justify-between";
     const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
-    const isAboveMediumScreens = useMediaQuery("min-width: 1060px;")
-    const navbarBackground = isTopOfPage ? "" : "bg-primary-100 drop-shadow"
+    const isAboveMediumScreens = useMediaQuery("(min-width: 1060px;)");
+    const navbarBackground = isTopOfPage ? "" : "bg-primary-100 drop-shadow";
   
     return (
     <nav>
