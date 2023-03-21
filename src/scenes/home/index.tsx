766b3e8e39a0bfc3e454317fea07275a531ed0dd
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-03-21 11:56:15 +0100

    add motion to Heading

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -7,6 +7,7 @@ import SponsorRedBull from '@/assets/SponsorRedbull.png';
 import SponsorForbes from '@/assets/SponsorForbes.png';
 import SponsorFortune from '@/assets/SponsorFortune.png';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
+import { motion } from 'framer-motion';
 
 type Props = {
     setSelectedPage: (value: SelectedPage) => void;
@@ -24,7 +25,17 @@ const Home = ({ setSelectedPage }: Props) => {
             {/* MAIN HEADER */}
             <div className="z-10 mt-32 md:basis-3/5">
                 {/* HEADINGS */}
-                <div className="md:-mt-20">
+                <motion.div 
+                className="md:-mt-20"
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true, amount: 0.5}}
+                transition={{ duration: 0.5}}
+                variants={{
+                    hidden: { opacity: 0, x: -50 },
+                    visible: { opacity: 1, x: 0},
+                }}
+                >
                     <div className="relative">
                         <div className="before:absolute before:-top-20 before:-left-20 before:z-[-1] md:before:content-evolvetext">
                             <img src={HomePageText} alt="home-page-text" />
@@ -33,7 +44,7 @@ const Home = ({ setSelectedPage }: Props) => {
                     <p className="mt-8 text-sm md:text-start">
                         Unrivaled Gym. Unparalleled Training Fitness Classes. World Class Studios to get the Body Shapes That you Dream of.. Get Your Dream Body Now.
                     </p>
-                </div>
+                </motion.div>
 
                 {/* ACTIONS */}
                 <div className="mt-8 flex items-center gp-8">
