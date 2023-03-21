141f14aba646d26fe9ba3cdae3e44e302e194ab5
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-03-21 09:51:26 +0100

    add structure to Home component

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -12,8 +12,30 @@ type Props = {
 }
 
 const Home = ({ setSelectedPage }: Props) => {
+    const isAboveMediumScreens = useMediaQuery("(min-width:1060px;)");
   return (
-    <div>Home</div>
+    <section
+    id="home"
+    className="gap-16 bg-grey-20 py-10 md:h-full md:pb-0"
+    >
+        {/* IMAGE AND MAIN HEADER */}
+        <div>
+            {/* MAIN HEADER */}
+            <div>
+                {/* HEADINGS */}
+                <div>
+                    <div>
+                        <div></div>
+                    </div>
+                </div>
+            </div>
+            {/* IMAGE */}
+        <div>
+
+        </div>
+
+        </div>
+    </section>
   )
 }
 
