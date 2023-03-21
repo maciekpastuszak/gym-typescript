6bf130f38a010393984ae0c1f76eeca870df6a3f
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-03-21 11:02:41 +0100

    add styles to headings in Home section

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -20,13 +20,13 @@ const Home = ({ setSelectedPage }: Props) => {
     className="gap-16 bg-grey-20 py-10 md:h-full md:pb-0"
     >
         {/* IMAGE AND MAIN HEADER */}
-        <div>
+        <div className="md:flex mx-auto w-5/6 items-center justify-center md:h-5/6">
             {/* MAIN HEADER */}
-            <div>
+            <div className="z-10 mt-32 md:basis-3/5">
                 {/* HEADINGS */}
-                <div>
-                    <div>
-                        <div>
+                <div className="md:-mt-20">
+                    <div className="relative">
+                        <div className="before:absolute before:-top-20">
                             <img src={HomePageText} alt="home-page-text" />
                         </div>
                     </div>
