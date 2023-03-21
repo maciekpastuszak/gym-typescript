c9592a0476812d6aa13068044449ee5f3eb91b75
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-03-21 11:46:39 +0100

    bugfix - sponsors

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -3,7 +3,7 @@ import ActionButton from '@/shared/ActionButton'
 import { SelectedPage } from '@/shared/types'
 import HomePageText from '@/assets/HomePageText.png';
 import HomePageGraphic from '@/assets/HomePageGraphic.png';
-import SponsorRedbull from '@/assets/SponsorRedbull.png';
+import SponsorRedBull from '@/assets/SponsorRedbull.png';
 import SponsorForbes from '@/assets/SponsorForbes.png';
 import SponsorFortune from '@/assets/SponsorFortune.png';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
@@ -13,7 +13,7 @@ type Props = {
 }
 
 const Home = ({ setSelectedPage }: Props) => {
-    const isAboveMediumScreens = useMediaQuery("(min-width:1060px;)");
+    const isAboveMediumScreens = useMediaQuery("(min-width:1060px)");
   return (
     <section
     id="home"
@@ -56,20 +56,19 @@ const Home = ({ setSelectedPage }: Props) => {
         </div>
 
         {/* SPONSORS */}
-
         {isAboveMediumScreens && (
-        <div className="h-[150px] w-full bg-primary-100 py-10">
-          <div className="mx-auto w-5/6">
-            <div className="flex w-3/5 items-center justify-between gap-8">
-              <img alt="redbull-sponsor" src={SponsorRedbull} />
-              <img alt="forbes-sponsor" src={SponsorForbes} />
-              <img alt="fortune-sponsor" src={SponsorFortune} />
-            </div>
-          </div>
+      <div className="h-[150px] w-full bg-primary-100 py-10">
+      <div className="mx-auto w-5/6">
+        <div className="flex w-3/5 items-center justify-between gap-8">
+          <img alt="redbull-sponsor" src={SponsorRedBull} />
+          <img alt="forbes-sponsor" src={SponsorForbes} />
+          <img alt="fortune-sponsor" src={SponsorFortune} />
         </div>
+      </div>
+    </div>
       )}
     </section>
-  )
-}
+  );
+};
 
 export default Home
