1e9904de0b7a30bf1648a7e40deb5b7f2c202a72
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-03-22 13:27:51 +0100

    add benefits const for Benefits section

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -3,6 +3,27 @@ import { SelectedPage } from "@/shared/types";
 import { HomeModernIcon, UserGroupIcon, AcademicCapIcon } from "@heroicons/react/24/solid";
 import { motion } from "framer-motion";
 
+const benefits = [
+    {
+        icon: <HomeModernIcon className="h-6 w-6" />,
+        title: "State of the Art Facilities",
+        description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur"
+    },
+    {
+        icon: <UserGroupIcon className="h-6 w-6" />,
+        title: "100's of Diverse Classes",
+        description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur"
+    },
+    {
+        icon: <AcademicCapIcon className="h-6 w-6" />,
+        title: "Expert and pro trainers",
+        description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur"
+    }
+]
+
 type Props = {
     setSelectedPage: (value: SelectedPage) => void;
 }
@@ -22,6 +43,11 @@ const Benefits = ({setSelectedPage}: Props) => {
             <p className="my-5 text-sm">We provide world class fitness equipment, trainers and classes to get to your ultimate fitness goals with ease. We provide true care into each and every member.</p>
         </div>
         {/* BENEFITS */}
+        <div className="md:flex items-center justify-between gap-8 mt-5">
+            {benefits.map((benefit) => (
+                <Benefit />
+            ))}
+        </div>
         </motion.div>
     </section>
   )
