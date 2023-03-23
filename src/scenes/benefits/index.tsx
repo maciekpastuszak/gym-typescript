0b1332cce4bdedcb932cd8840e2f4c08b15ba49a
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-03-23 12:38:40 +0100

    add motion to header

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -46,10 +46,19 @@ const Benefits = ({setSelectedPage}: Props) => {
          onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
         >
             {/* HEADER */}
-        <div className="md:my-5 md:w-3/5">
+        <motion.div 
+        className="md:my-5 md:w-3/5"
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.5}}
+        transition={{ duration: 0.5}}
+        variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0},
+        }}>
             <HText>MORE THAN JUST A GYM.</HText>
             <p className="my-5 text-sm">We provide world class fitness equipment, trainers and classes to get to your ultimate fitness goals with ease. We provide true care into each and every member.</p>
-        </div>
+        </motion.div>
         {/* BENEFITS */}
         <motion.div 
         className="md:flex items-center justify-between gap-8 mt-5"
