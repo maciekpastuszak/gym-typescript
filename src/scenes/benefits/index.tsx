4c37d9618a990a2214122dffefd42f55e0dde5a1
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-03-23 22:04:25 +0100

    add motion to title part of Benefits section

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -91,12 +91,20 @@ const Benefits = ({setSelectedPage}: Props) => {
                 {/* TITLE */}
                 <div className="relative">
                     <div className="before:absolute before:-20-20 before:-left-20 before:z-[1] before:content-abstractwaves">
-                        <div>
+                        <motion.div
+                        initial="hidden"
+                        whileInView="visible"
+                        viewport={{ once: true, amount: 0.5}}
+                        transition={{ duration: 0.5}}
+                        variants={{
+                            hidden: { opacity: 0, x: 50 },
+                            visible: { opacity: 1, x: 0},
+                        }}>
                             <HText>
                                 MILLIONS OF HAPPY MEMBERS GETTING {" "}
                                 <span className="text-primary-500">FIT</span>
                             </HText>
-                        </div>
+                        </motion.div>
                     </div>
                 </div>
                 {/* DESCRIPTION */}
