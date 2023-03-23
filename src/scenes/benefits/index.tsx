9d4e15d7d08f0937fbcfe9fdb724bd108e104fae
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-03-23 22:05:32 +0100

    add motion to description of Benefits section

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -108,10 +108,18 @@ const Benefits = ({setSelectedPage}: Props) => {
                     </div>
                 </div>
                 {/* DESCRIPTION */}
-                    <div>
+                    <motion.div
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true, amount: 0.5}}
+                    transition={{ delay: 0.2, duration: 0.5}}
+                    variants={{
+                        hidden: { opacity: 0, x: -50 },
+                        visible: { opacity: 1, x: 0},
+                    }}>
                         <p className="my-5">Donec tincidunt nec sem sed tincidunt. Curabitur nec ullamcorper metus. Quisque id leo imperdiet dui molestie tincidunt sed a est. Nulla gravida eleifend lorem non mattis. Ut hendrerit non arcu vitae malesuada. Integer sit amet semper lorem. Nam sollicitudin augue a pulvinar laoreet. Duis sed tincidunt nisl. Phasellus vestibulum euismod tristique. Vivamus in tellus vitae orci porta porttitor at vel neque. Praesent tincidunt molestie odio. Aenean volutpat odio venenatis rutrum feugiat. Donec ullamcorper sem pulvinar massa condimentum scelerisque. Mauris auctor finibus mauris a ornare. Ut a purus ut leo varius blandit. Nullam at vehicula ante</p>
                         <p className="my-5">Ut eget aliquet magna, at pulvinar ex. Suspendisse malesuada egestas arcu id aliquet. Suspendisse massa nisl, suscipit eu ex vitae, consequat mollis risus. Nullam dui ligula, sollicitudin sit amet elementum nec, sollicitudin ac ipsum. Fusce vitae nunc quis nulla tempor vestibulum sed non nisl.</p>
-                    </div>
+                    </motion.div>
                 {/* BUTTON */}
                 <div className="relative mt-16">
                     <div className="before:absolute before:-bottom-20 before:right-40 before:z-[-1] before:content-sparkles ">
