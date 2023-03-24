08b99bf45991fc02cc443943bb8a439d41ecfbfe
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-03-24 09:39:22 +0100

    add missing classes to p tags in Class component

--- src/scenes/ourClasses/Class.tsx
+++ src/scenes/ourClasses/Class.tsx
@@ -16,8 +16,8 @@ const Class = ({ name, description, image }: Props) => {
     return (
   <li className="relative mx-5 inline-block h-[380px] w-[450px]">
     <div className={overlayStyles}>
-        <p>{name}</p>
-        <p>{description}</p>
+        <p className="text-2xl">{name}</p>
+        <p className="mt-5">{description}</p>
         <img src={image} alt={`${image}`} />
     </div>
   </li>
