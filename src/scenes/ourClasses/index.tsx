99b41391e0a1c2d356f2fb5f6e0c3a89d2e355d4
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-03-24 08:53:22 +0100

    add main section animations

--- src/scenes/ourClasses/index.tsx
+++ src/scenes/ourClasses/index.tsx
@@ -5,13 +5,32 @@ import image3 from '@assets/image3.png';
 import image4 from '@assets/image4.png';
 import image5 from '@assets/image5.png';
 import image6 from '@assets/image6.png';
+import { motion } from 'framer-motion';
 
 type Props = {
     setSelectedPage:(value: SelectedPage) => void;
 }
 
 const OurClasses = ({ setSelectedPage } : Props) => {
-  return <div>OurClasses</div>;
+  return <section id="ourclasses" className="w-full bg-primary-100 py-40 ">
+    <motion.div
+    onViewportEnter={() => setSelectedPage(SelectedPage.OurClasses)}
+    >
+        <motion.div
+        className="mx-auto w-5/6"
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.5}}
+        transition={{ duration: 0.5}}
+        variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0},
+        }}
+        >
+
+        </motion.div>
+    </motion.div>
+    </section>;
 };
 
 export default OurClasses;
