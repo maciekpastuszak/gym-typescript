a981ac98a044aa1b6c2893a4590de68369d57a01
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-03-25 15:12:20 +0100

    bugfix - adjust motion

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -39,8 +39,8 @@ const ContactUs = ({setSelectedPage}: Props) => {
             viewport={{ once: true, amount: 0.5}}
             transition={{ duration: 0.5}}
             variants={{
-                hidden: { opacity: 0, x: -50 },
-                visible: { opacity: 1, x: 0},
+                hidden: { opacity: 0, y: 50 },
+                visible: { opacity: 1, y: 0},
             }}
             >
 
