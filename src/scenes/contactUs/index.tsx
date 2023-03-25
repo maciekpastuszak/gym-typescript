34e9e979b0d8d28414df9cc841e7ffdc45813bf1
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-03-25 15:04:02 +0100

    add motion to header in ContactUs section

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -11,7 +11,19 @@ const ContactUs = ({setSelectedPage}: Props) => {
   return <section>
     <motion.div onViewportEnter={() => setSelectedPage(SelectedPage.ContactUs)}>
         {/* HEADER */}
-        <motion.div></motion.div>
+        <motion.div
+            className="md:w-3/5"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5}}
+            transition={{ duration: 0.5}}
+            variants={{
+                hidden: { opacity: 0, x: -50 },
+                visible: { opacity: 1, x: 0},
+            }}
+        >
+
+        </motion.div>
     </motion.div>
   </section>
 }
