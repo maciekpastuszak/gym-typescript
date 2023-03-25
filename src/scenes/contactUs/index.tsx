ddbed191088099ec510b969bd636135f1620a1de
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-03-25 15:08:59 +0100

    add header with paragraph into ContactUs

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -2,6 +2,7 @@ import { useForm } from "react-hook-form";
 import { SelectedPage } from "@/shared/types";
 import { motion } from "framer-motion";
 import ContactUsPageGraphic from "@/assets/ContactUsPageGraphic.png";
+import HText from "@/shared/HText";
 
 type Props = {
     setSelectedPage: (value: SelectedPage) => void;
@@ -22,7 +23,12 @@ const ContactUs = ({setSelectedPage}: Props) => {
                 visible: { opacity: 1, x: 0},
             }}
         >
-
+            <HText>
+                <span className="text-primary-500">JOIN NOW</span> TO GET IN SHAPE
+            </HText>
+            <p className="my-5">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+            </p>
         </motion.div>
     </motion.div>
   </section>
