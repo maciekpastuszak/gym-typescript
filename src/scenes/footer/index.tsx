31ba9154b2e6f9a59e20b43830d5571065094715
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-03-26 11:44:30 +0200

    add colum with img and ARR to footer

--- src/scenes/footer/index.tsx
+++ src/scenes/footer/index.tsx
@@ -1,8 +1,18 @@
 import Logo from "@/assets/Logo.png"
 
 const Footer = () => {
-  return <footer>
-    
+  return <footer className="bg-primary-100 py-16">
+    <div className="justify-content mx-auto w-5/6 gap-16 md:flex">
+        <div className="mt-16 basis-1/2 md:mt-0">
+            <img src="logo" alt="logo" />
+            <p className="my-5">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            </p>
+            <p>C Evogym All Rights Reserved</p>
+        </div>
+        <div></div>
+        <div></div>
+    </div>
   </footer>;
 };
 
