bf8c9f85c4df3dc050aeabe6bcb76f4735d552d7
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-03-27 09:11:44 +0200

    bugfix - update footer styles

--- src/scenes/footer/index.tsx
+++ src/scenes/footer/index.tsx
@@ -4,11 +4,11 @@ const Footer = () => {
   return <footer className="bg-primary-100 py-16">
     <div className="justify-content mx-auto w-5/6 gap-16 md:flex">
         <div className="mt-16 basis-1/2 md:mt-0">
-            <img src="logo" alt="logo" />
+            <img src={Logo} alt="logo" />
             <p className="my-5">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
             </p>
-            <p>C Evogym All Rights Reserved</p>
+            <p>© Evogym All Rights Reserved</p>
         </div>
         <div className="mt-16 basis-1/4 md:mt-0">
             <h4 className="font-bold">Links</h4>
