8577122a5d68ca627b7e1f1e2983cb2ce093c14e
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2025-09-19 12:05:10 +0200

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [{find: "@", replacement: path.resolve(__dirname, "src")}]
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
   build: {
     // generate manifest.json in outDir
     manifest: true,
-    rollupOptions: {
-      // overwrite default .html entry
-      input: './src/main.tsx',
-    },
   },
-})
+});
