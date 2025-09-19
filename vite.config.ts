0eecedde26c763e177564533cd52bffd762efad1
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2025-09-19 12:19:41 +0200

    vite reconfig

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => ({
+  plugins: [
+    react({
+      // Disable React Fast Refresh when building for production
+      fastRefresh: mode !== "production",
+    }),
+  ],
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
   build: {
-    // generate manifest.json in outDir
     manifest: true,
   },
-});
+}));
